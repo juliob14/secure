0999a1db22e4cb2c6368c2b451324f002ef74d2f
juliob14 <assassinpoint@gmail.com>
2015-10-21 19:51:26 -0600

    otro commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,10 +6,27 @@ import play.mvc.*;
 import java.util.*;
 
 import models.*;
+import play.data.validation.Check;
+@With(Secure.class)
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        String user = Security.connected();
+        render(user);
+        
+        
     }
-
+    @Check("administrator")
+   public static void delete(Long id) {
+   }
+    static boolean check(String profile) {
+        User user = User.find("byEmail", connected()).first();
+        if ("administrator".equals(profile)) {
+            return user.admin;
+        }
+        else {
+            return false;
+        }
+    }    
+    
 }
--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controllers;
 
-/**
- *
- * @author JULIO
- */
-public class Security {
+
+public class Security extends Secure.Security {
     
+    static boolean authenticate(String username, String password) {
+        User user = User.find("byEmail", username).first();
+        return user != null && user.password.equals(password);
+    }
 }
